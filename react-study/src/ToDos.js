019bde02c286d91e005189425e28c8e11196912e
junghye <wjdgp1398@gmail.com>
2024-10-25 12:55:57 +0900

    241025 React Study

---
+++ react-study/src/ToDos.js
@@ -0,0 +1,30 @@
+import {useState} from "react";
+
+function ToDos() {
+    const [toDo, setTodo] = useState("");
+    const [toDos, setTodos] = useState([]);
+    const onChangeValue = (event) => setTodo(event.target.value);
+    const onSubmit = (event) => {
+        event.preventDefault();
+        // console.log(toDo);
+        if (toDo === ""){
+            return;
+        }
+        // Const ToDos 이기때문에 toDos.push 또는 ToDo = '' 와 같이 직접값을 줄수없고 함수로 값을 전달해야 함
+        // const food = [1,2,3]; 4를 추가하고싶은 경우, const a = [5, ...food]; → [5,1,2,3]; 같은 라인에 복사
+        setTodos(currentArray => [toDo, ...currentArray]);
+        setTodo("");
+    }
+    // console.log(toDos);
+    return (
+        <div>
+            <h1>My To Dos ({toDos.length})</h1>
+            <form onSubmit={onSubmit}>
+                <input type="text" placeholder="Write your to do..." value={toDo} onChange={onChangeValue} />&nbsp;
+                <button>Add To Do</button>
+            </form>
+        </div>
+    );
+}
+
+export default ToDos;
---
+++ react-study/src/UseGuide.js
@@ -0,0 +1,87 @@
+// import PropTypes from 'prop-types';
+import {useState, useEffect} from "react";
+
+function Hello(){
+    /*useEffect(() => {
+        console.log("created :)");
+        // cleanup function
+        return() => console.log("destroyed :(");
+    }, []);*/
+    useEffect(() => {
+        console.log("hi :)");
+        return () => console.log("bye :(");
+    }, []);
+    useEffect(function(){
+        console.log("hi :)");
+        return function (){
+            console.log("bye :(")
+        };
+    }, []);
+    // return <h1>Hello</h1>;
+    return;
+}
+
+// Hello 와 같은걸 풀어쓴것
+/*function HelloLong(){
+    function byFn(){
+        console.log("bye :(");
+    }
+    function hiFn (){
+        console.log("hi :)");
+        return byFn;
+    }
+    return <h1>Hi</h1>
+}*/
+
+function UseGuide(){
+    const [counter, setValue] = useState(0);
+    const [keyword, setKeyword] = useState("");
+    const onClick = () => setValue((prev) => prev+1);
+    const onChange = (event) => setKeyword(event.target.value);
+    // console.log("i run all the time");
+    // useEffect : 랜더링을 컨트롤 할수있는 기능, [] 가 비어있으면 1회만 실행하지만 [] 안에 명령어 기준으로 작동
+    /*const iRunOnlyOnce = () => {
+        console.log("CALL THE API....");
+    };
+    useEffect(iRunOnlyOnce, []);*/
+
+    // useEffect 사용법
+    /*useEffect(() => {
+        console.log("i run only once");
+    }, []);
+    useEffect(() => {
+        /!*if(keyword !== "" && keyword.length > 5){
+            console.log("SEARCH FOR", keyword);
+        }*!/
+        console.log("i run when 'keyword' changes.");
+    }, [keyword]);
+    useEffect(() => {
+        console.log("i run when 'counter' changes.");
+    }, [counter]);
+    useEffect(() => {
+        console.log("i run when keyword & counter changes.");
+    }, [keyword, counter]);*/
+
+    const [showing, setShowing] = useState(false);
+    const onClickBtn = () => setShowing(prev => !prev);
+
+    return (
+        <div>
+            <input type="text" placeholder="Serach here..." onChange={onChange} value={keyword}/>
+            <div>
+                <h2 style={{display: "inline-block"}}>Click Count : {counter}</h2>&nbsp;
+                <button onClick={onClick}>click me</button>
+            </div>
+
+            {showing ? <Hello/> : null}
+            {/*{showing ? <HelloLong /> : null}*/}
+            <button onClick={onClickBtn}>{showing ? "Hide" : "Show"}</button>
+        </div>
+    );
+}
+
+/*UseGuide.propTypes = {
+    text: PropTypes.string.isRequired
+}*/
+
+export default UseGuide;
