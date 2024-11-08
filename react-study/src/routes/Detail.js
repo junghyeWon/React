973f55a529592e852f4e17e22a27562be236b226
junghye <wjdgp1398@gmail.com>
2024-11-08 12:21:43 +0900

    241108 React Study Done

--- react-study/src/routes/Detail.js
+++ react-study/src/routes/Detail.js
@@ -1,23 +1,39 @@
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 
 function Detail(){
     const {id} = useParams(); // Route 로 넘겨받은 파라미터 값
+    const [loading, setLoading] = useState(true);
     const [movie, setMovie] = useState([]);
     const getMovie = async() => {
         const json = await (
             await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
         ).json();
+        // console.log(id);
         setMovie(json.data.movie);
-    }
+        setLoading(false);
+    };
     useEffect(() => {
         getMovie();
     }, []);
-
-    console.log(movie);
-
+    // console.log(movie)
     return (
-        <h1>Detail</h1>
+        <div>
+            {loading ? (<strong>Loading...</strong>) : (
+                <div>
+                    <img src={movie.medium_cover_image} alt={movie.title}/>
+                    <div>
+                        <strong><Link to={movie.url} target="_blank">{movie.title}</Link></strong><em>({movie.year})</em>
+                        <span>{movie.description_full}</span>
+                        <ul>
+                            {movie.genres.map((g) => (
+                                <li key={g}>{g}</li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            )}
+        </div>
     );
 }
 
