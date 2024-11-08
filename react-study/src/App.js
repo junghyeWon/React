769d932449ffdd2b3b4a5c099fcf989d3144da59
junghye <wjdgp1398@gmail.com>
2024-11-08 16:27:04 +0900

    241108 React Study Done

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -7,11 +7,10 @@ function App() {
     return (
         // ex) BrowserRouter : http://localhost:3000/movie HashRouter : http://localhost:3000/#/movie
         // path 뒤에 넘겨주는 값이 궁금하다면, :xxxx 로 넘겨주면 useParams 로 받을수 있다
-        // basename={process.env.PUBLIC_URL}
         <BrowserRouter>
-            <Routes basename={process.env.PUBLIC_URL}>
+            <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/movie:id" element={<Detail />} />
+                <Route path="/movie/:id" element={<Detail />} />
                 <Route path="/hello" element={<h1>hello</h1>} />
             </Routes>
         </BrowserRouter>
